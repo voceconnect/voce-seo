04b6a1029124e545e7c61415a6f5847fe6e975fe
Mat Gargano <mgargano@gmail.com>
2014-03-21 16:52:36 -0400

    Adding unit tests (note they are broken)

---
+++ tests/test_voce_seo.php
@@ -0,0 +1,15 @@
+<?php
+
+class Voce_SEO_Tests extends WP_UnitTestCase {
+ 
+    
+	function test_false_is_false() {
+        $this->assertFalse( false );
+    } 
+    
+
+    function test_true_is_true() {
+        $this->assetTrue( true );
+    } 
+
+} 
